9898fe5dcd53200195724f5df23d1ad683589c19
Mario Rodrigues <marioro2@c1r10s3.42lisboa.com>
2025-12-11 16:27:25 +0000

    nothing

--- src/execution/executor/exec_main.c
+++ src/execution/executor/exec_main.c
@@ -1,11 +1,11 @@
 #include "../../../includes/minishell.h"
 
-// cmd[0]:  command = "ls"
-//          args = ["ls", "-l", NULL]
-//          redirs -> lista com <, >, >>, << (heredoc)
-//          next == pipe para próximo comando
+cmd[0]:  command = "ls"
+         args = ["ls", "-l", NULL]
+         redirs -> lista com <, >, >>, << (heredoc)
+         next == pipe para próximo comando
 
-// cmd[1]:  command = "grep"
-//          args = ["grep", "txt", NULL]
-//          redirs -> lista com <, >, >>, << (heredoc)
-// 		 next == NULL
+cmd[1]:  command = "grep"
+         args = ["grep", "txt", NULL]
+         redirs -> lista com <, >, >>, << (heredoc)
+		 next == NULL

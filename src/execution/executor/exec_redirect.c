3682873bf7ff347eb1900f2fccbef25e5bed68e5
Mário Rodrigues <marioandrerodrigues@hotmail.com>
2025-12-13 15:06:33 +0000

    nothing

---
+++ src/execution/executor/exec_redirect.c
@@ -0,0 +1,13 @@
+#include "../../../includes/minishell.h"
+
+// Funções para:
+
+// aplicar <
+
+// aplicar >
+
+// aplicar >>
+
+// aplicar << (heredoc output para fd temporário)
+
+// int apply_redirs(t_cmd *cmd);
---
+++ src/execution/executor/exec_utils.c
@@ -0,0 +1,8 @@
+#include "../../../includes/minishell.h"
+
+// int is_builtin(char *cmd);
+// int run_builtin_in_child(t_shell *shell, t_cmd *cmd);
+// int run_builtin_in_parent(t_shell *shell, t_cmd *cmd);
+
+// if strcmp(cmd, "cd") == 0 -> ft_cd()
+// if strcmp(cmd, "echo") == 0 -> ft_echo()

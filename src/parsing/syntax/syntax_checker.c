d6d5fbe3def40e54f5a84c43c70be9879ef57422
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2025-12-17 19:58:22 +0000

    Add syntax folder and .c files

---
+++ src/parsing/syntax/syntax_checker.c
@@ -0,0 +1,95 @@
+#include "../../../includes/minishell.h"
+
+static int	is_redir(t_token_type type)
+{
+	return (type == TOKEN_REDIR_IN || type == TOKEN_REDIR_OUT
+		|| type == TOKEN_REDIR_APPEND || type == TOKEN_REDIR_HEREDOC);
+}
+
+static void	print_syntax_error(t_token *token)
+{
+	ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+	if (!token)
+		ft_putstr_fd("newline", 2);
+	else if (token->type == TOKEN_PIPE)
+		ft_putstr_fd("|", 2);
+	else if (token->type == TOKEN_REDIR_IN)
+		ft_putstr_fd("<", 2);
+	else if (token->type == TOKEN_REDIR_OUT)
+		ft_putstr_fd(">", 2);
+	else if (token->type == TOKEN_REDIR_APPEND)
+		ft_putstr_fd(">>", 2);
+	else if (token->type == TOKEN_REDIR_HEREDOC)
+		ft_putstr_fd("<<", 2);
+	ft_putstr_fd("'\n", 2);
+}
+
+static int	check_redir_syntax(t_token *token)
+{
+	if (!token->next)
+	{
+		print_syntax_error(NULL);
+		return (0);
+	}
+	else if (token->next->type == TOKEN_PIPE)
+	{
+		print_syntax_error(token->next);
+		return (0);
+	}
+	else if (is_redir(token->next->type))
+	{
+		print_syntax_error(token->next);
+		return (0);
+	}
+	else if (token->next->type != TOKEN_WORD)
+	{
+		print_syntax_error(token->next);
+		return (0);
+	}
+	return (1);
+}
+
+static int	check_pipe_syntax(t_token *token, t_token *prev)
+{
+	if (!prev)
+	{
+		print_syntax_error(token);
+		return (0);
+	}
+	else if (!token->next)
+	{
+		print_syntax_error(NULL);
+		return (0);
+	}
+	else if (token->next->type == TOKEN_PIPE)
+	{
+		print_syntax_error(token->next);
+		return (0);
+	}
+	return (1);
+}
+
+int	check_syntax(t_token *tokens)
+{
+	t_token	*current;
+	t_token	*prev;
+
+	current = tokens;
+	prev = NULL;
+	while (current)
+	{
+		if (is_redir(current->type))
+		{
+			if (!check_redir_syntax(current))
+				return (0);
+		}
+		else if (current->type == TOKEN_PIPE)
+		{
+			if (!check_pipe_syntax(current, prev))
+				return (0);
+		}
+		prev = current;
+		current = current->next;
+	}
+	return (1);
+}

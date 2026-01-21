c8f5fb0dc8766c4d22cec961d16fa2488dd46a1d
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2026-01-21 15:52:41 +0000

    Add temporary changes (comments) to builtin_exit.c and heredoc.c files for the tester to run

--- src/execution/builtins/builtin_exit.c
+++ src/execution/builtins/builtin_exit.c
@@ -58,3 +58,35 @@ int	ft_exit(char **args)
 	shell()->exit_code = (unsigned char)code;
 	return ((unsigned char)code);
 }
+
+// int	ft_exit(char **args)
+// {
+// 	long	code;
+
+// 	if (isatty(STDIN_FILENO))
+// 		ft_putstr_fd("exit\n", 1);
+// 	if (!args[1])
+// 	{
+// 		shell()->should_exit = 1;
+// 		return (shell()->exit_code);
+// 	}
+
+// 	if (!parse_exit_code(args[1], &code))
+// 	{
+// 		ft_putstr_fd("minishell: exit: ", 2);
+// 		ft_putstr_fd(args[1], 2);
+// 		ft_putstr_fd(": numeric argument required\n", 2);
+// 		shell()->should_exit = 1;
+// 		shell()->exit_code = 2;
+// 		return (2);
+// 	}
+// 	if (args[2])
+// 	{
+// 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+// 		shell()->exit_code = 1;
+// 		return (1);
+// 	}
+// 	shell()->should_exit = 1;
+// 	shell()->exit_code = (unsigned char)code;
+// 	return ((unsigned char)code);
+// }

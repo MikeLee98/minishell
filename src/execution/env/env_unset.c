ed2e44ef5dfb9dc856ec7018e52a61d222894bca
Mário Rodrigues <marioandrerodrigues@hotmail.com>
2026-01-22 18:59:45 +0000

    norm fixes

--- src/execution/env/env_unset.c
+++ src/execution/env/env_unset.c
@@ -6,33 +6,33 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 19:55:42 by mario             #+#    #+#             */
-/*   Updated: 2026/01/21 19:55:43 by mario            ###   ########.fr       */
+/*   Updated: 2026/01/22 18:59:05 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-void    env_unset(t_env **env, char *key)
+void	env_unset(t_env **env, char *key)
 {
-    t_env *tmp;
-    t_env *prev;
+	t_env	*tmp;
+	t_env	*prev;
 
-    tmp = *env;
-    prev = NULL;
-    while (tmp)
-    {
-        if (ft_strcmp(tmp->key, key) == 0)
-        {
-            if (prev)
-                prev->next = tmp->next;
-            else
-                *env = tmp->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-            return;
-        }
-        prev = tmp;
-        tmp = tmp->next;
-    }
+	tmp = *env;
+	prev = NULL;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->key, key) == 0)
+		{
+			if (prev)
+				prev->next = tmp->next;
+			else
+				*env = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			return ;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
 }
--- src/execution/env/env_utils.c
+++ src/execution/env/env_utils.c
@@ -6,95 +6,91 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 19:55:46 by mario             #+#    #+#             */
-/*   Updated: 2026/01/21 19:55:47 by mario            ###   ########.fr       */
+/*   Updated: 2026/01/22 18:58:45 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-t_env   *env_find(t_env *env, char *key)
+t_env	*env_find(t_env *env, char *key)
 {
-    while (env)
-    {
-        if (ft_strcmp(env->key, key) == 0)
-            return (env);
-        env = env->next;
-    }
-    return (NULL);
+	while (env)
+	{
+		if (ft_strcmp(env->key, key) == 0)
+			return (env);
+		env = env->next;
+	}
+	return (NULL);
 }
 
-char    *ft_getenv(t_env *env, char *key)
+char	*ft_getenv(t_env *env, char *key)
 {
-    t_env *node;
+	t_env	*node;
 
-    node = env_find(env, key);
-    if (node)
-        return (node->value);
-    return (NULL);
+	node = env_find(env, key);
+	if (node)
+		return (node->value);
+	return (NULL);
 }
 
-static char *join_key_value(char *key, char *value)
+static char	*join_key_value(char *key, char *value)
 {
-    char *tmp;
-    char *res;
+	char	*tmp;
+	char	*res;
 
 	if (!value)
-        return (ft_strdup(key));
-    tmp = ft_strjoin(key, "=");
-    if (!tmp)
-        return (NULL);
-    res = ft_strjoin(tmp, value);
-    free(tmp);
-    return (res);
+		return (ft_strdup(key));
+	tmp = ft_strjoin(key, "=");
+	if (!tmp)
+		return (NULL);
+	res = ft_strjoin(tmp, value);
+	free(tmp);
+	return (res);
 }
 
-char    **env_to_array(t_env *env)
+char	**env_to_array(t_env *env)
 {
-    char    **envp;
-    int     count;
-    int     i;
-	t_env   *head;
+	char	**envp;
+	int		count;
+	int		i;
+	t_env	*tmp;
 
-	head = env;
-    count = 0;
-    while (env)
-    {
-        count++;
-        env = env->next;
-    }
-    envp = malloc(sizeof(char *) * (count + 1));
-    if (!envp)
-        return (NULL);
-    i = 0;
-	env = head;
-    while (env)
-    {
-        envp[i] = join_key_value(env->key, env->value);
+	tmp = env;
+	count = 0;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	envp = malloc(sizeof(char *) * (count + 1));
+	if (!envp)
+		return (NULL);
+	i = 0;
+	while (env)
+	{
+		envp[i] = join_key_value(env->key, env->value);
 		if (!envp[i])
-		{
-			free_env_array(envp);
-			return (NULL);
-		}
-        i++;
-        env = env->next;
-    }
-    envp[i] = NULL;
-    return (envp);
+			return (free_env_array(envp), NULL);
+		i++;
+		env = env->next;
+	}
+	return (envp[i] = NULL, envp);
 }
 
-void update_shlvl(void)
+void	update_shlvl(void)
 {
-    char *shlvl_str = ft_getenv(shell()->env, "SHLVL");
-    int shlvl = 1;
+	char	*shlvl_str;
+	int		shlvl;
+	char	buffer[16];
 
-    if (shlvl_str)
-    {
-        shlvl = ft_atoi(shlvl_str) + 1;
-        if (shlvl < 0)
-            shlvl = 0;
-    }
-
-    char buffer[16];
-    ft_itoa_buffer(shlvl, buffer);
-    env_set(&shell()->env, "SHLVL", buffer, 0);
+	shlvl_str = ft_getenv(shell()->env, "SHLVL");
+	shlvl = 1;
+	if (shlvl_str)
+	{
+		shlvl = ft_atoi(shlvl_str) + 1;
+		if (shlvl < 0)
+			shlvl = 0;
+	}
+	ft_itoa_buffer(shlvl, buffer);
+	env_set(&shell()->env, "SHLVL", buffer, 0);
 }

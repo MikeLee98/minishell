cc9fc70d52cedee607c7e13876ef8668133cf05f
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2026-02-03 15:00:57 +0000

    Fix unset arguments error message

--- src/execution/builtins/builtin_unset.c
+++ src/execution/builtins/builtin_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_unset.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
+/*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 19:56:28 by mario             #+#    #+#             */
-/*   Updated: 2026/01/21 20:03:29 by mario            ###   ########.fr       */
+/*   Updated: 2026/02/03 14:51:38 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,20 @@ int	ft_unset(char **args)
 	i = 1;
 	while (args[i])
 	{
+		if (args[i][0] == '-' && args[i][1] != '\0')
+		{
+			if (args[i][1] == '-' && args[i][2] == '\0')
+			{
+				i++;
+				continue ;
+			}
+			ft_putstr_fd("unset: -", 2);
+			ft_putchar_fd(args[i][1], 2);
+			ft_putstr_fd(": invalid option\n", 2);
+			ft_putstr_fd("unset: usage: unset [-f] [-v] [-n] [name ...]\n", 2);
+			shell()->exit_code = 2;
+			return (2);
+		}
 		if (env_find(shell()->env, args[i]))
 			env_unset(&shell()->env, args[i]);
 		i++;

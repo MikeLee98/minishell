6453b5500838c56c6f8fc5365c5859b0cc011320
Mário Rodrigues <marioandrerodrigues@hotmail.com>
2026-02-03 21:45:05 +0000

    nothing

--- src/execution/builtins/builtin_env.c
+++ src/execution/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 19:56:06 by mario             #+#    #+#             */
-/*   Updated: 2026/02/02 21:06:50 by mario            ###   ########.fr       */
+/*   Updated: 2026/02/03 21:44:19 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int	ft_env(void)
 {
 	t_env	*tmp;
 
-    if (shell()->cmds->args[1])
-    {
+	if (shell()->cmds->args[1])
+	{
 		ft_putstr_fd("env: ‘", 2);
 		ft_putstr_fd(shell()->cmds->args[1], 2);
 		ft_putstr_fd("’: No such file or directory\n", 2);
-        return (127);
-    }
+		return (127);
+	}
 	tmp = shell()->env;
 	while (tmp)
 	{

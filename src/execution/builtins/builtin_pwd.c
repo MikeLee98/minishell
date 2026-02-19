a1e6ca0ec91c4397da230288e41b4de58235b23e
Mário Rodrigues <marioandrerodrigues@hotmail.com>
2026-02-19 14:38:34 +0000

    pwd fix

--- src/execution/builtins/builtin_pwd.c
+++ src/execution/builtins/builtin_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_pwd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 19:56:25 by mario             #+#    #+#             */
-/*   Updated: 2026/02/05 22:40:11 by migusant         ###   ########.fr       */
+/*   Updated: 2026/02/19 14:38:27 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,14 @@ int	ft_pwd(void)
 {
 	char	*cwd;
 
-	cwd = getcwd(NULL, 0);
+	cwd = getcwd(NULL, 4096);
 	if (!cwd)
 	{
 		ft_putstr_fd("minishell: pwd: error retrieving current ", 2);
 		ft_putstr_fd("directory\n", 2);
-		shell()->exit_code = 1;
 		return (1);
 	}
 	ft_printf("%s\n", cwd);
 	free(cwd);
-	shell()->exit_code = 0;
 	return (0);
 }
